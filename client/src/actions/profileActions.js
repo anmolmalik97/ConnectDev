00f93942c3abd0d6296ddd18d4f234ca0a0ec0b2
Anmol Malik <anmolmalik@Anmols-MacBook-Pro.local>
2018-08-04 12:55:55 +0530

    profile adding completed

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -23,9 +23,20 @@ export const setProfileLoading = () => {
 	}
 }
 
+// create profile
+export const createProfile = (profileData,history) =>dispatch => {
+	axios.post('/api/profile',profileData)
+		.then(res => history.push('dashboard'))
+		.catch(err=> dispatch({
+			type: actionTypes.GET_ERRORS,
+			payload: err.response.data
+		}))
+}
+
 // clear profile
 export const clearCurrentProfile = () => {
 	return {
 		type: actionTypes.CLEAR_CURRENT_PROFILE
 	}
 }
+
