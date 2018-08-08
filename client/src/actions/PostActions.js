ffb4f1fca89c84244b53a9a79bbe9e0887cbdec2
Anmol Malik <anmolmalik@Anmols-MacBook-Pro.local>
2018-08-08 16:41:45 +0530

    added comments and like option

--- client/src/actions/PostActions.js
+++ client/src/actions/PostActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as actionTypes from './types';
 
-
+// add post
 export const addPost => postData => dispatch => {
 	axios.post('/api/posts',postData)
 		.then(res => dispatch({
@@ -12,4 +12,91 @@ export const addPost => postData => dispatch => {
 			type: actionTypes.GET_ERRORS,
 			payload: err.response.data
 		}))
+}
+
+
+// get posts
+export const getPosts => () => dispatch => {
+	dispatch(setPostLoading());
+	axios.get('/api/posts')
+		.then(res => dispatch({
+			type: actionTypes.GET_POSTS,
+			payload: res.data
+		}))
+		.catch(err => dispatch({
+			type: GET_POSTS,
+			payload: null
+		}))
+}
+
+// get post
+export const getPost => id => dispatch => {
+	dispatch(setPostLoading());
+	axios.get(`/api/posts/${id}`)
+		.then(res => dispatch({
+			type: actionTypes.GET_POST,
+			payload: res.data
+		}))
+		.catch(err => dispatch({
+			type: GET_POST,
+			payload: null
+		}))
+}
+
+// delete post
+export const deletePost => id => dispatch => {
+	axios.delete(`/api/posts/${id}`)
+		.then(res => dispatch({
+			type: actionTypes.DELETE_POST,
+			payload: id
+		}))
+		.catch(err => dispatch({
+			type: actionTypes.GET_ERRORS,
+			payload: err.response.data
+		}))
+}
+
+// add like
+
+export const addLike => id => dispatch => {
+	axios.post(`/api/posts/like/${id}`)
+		.then(res => dispatch(getPosts()))
+		.catch(err => dispatch({
+			type: actionTypes.GET_ERRORS,
+			payload: err.response.data
+		}))
+}
+
+// remove like
+
+export const removeLike => id => dispatch => {
+	axios.post(`/api/posts/unlike/${id}`)
+		.then(res => dispatch(getPosts()))
+		.catch(err => dispatch({
+			type: actionTypes.GET_ERRORS,
+			payload: err.response.data
+		}))
+}
+
+// add comment
+
+// add post
+export const addPost => postData => dispatch => {
+	axios.post('/api/posts',postData)
+		.then(res => dispatch({
+			type: actionTypes.ADD_POST,
+			payload: res.data
+		}))
+		.catch(err => dispatch({
+			type: actionTypes.GET_ERRORS,
+			payload: err.response.data
+		}))
+}
+
+// set loading state
+
+export const setPostLoading = () => {
+	return {
+		type: actionTypes.POST_LOADING
+	}
 }
